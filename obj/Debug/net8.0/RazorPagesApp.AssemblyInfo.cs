2e0e44fcdfda1f5497643c0fe9d31e134a90510a
Danny Calise <danny.calise@us.kbr.com>
2024-06-26 18:38:53 -0400

    added design

--- obj/Debug/net8.0/RazorPagesApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/RazorPagesApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5c298c927ab017777e41eaf6255f8a12df34c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
