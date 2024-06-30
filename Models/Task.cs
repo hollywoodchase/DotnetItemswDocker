287bc1b84d25726894e06e1cde9c7690b1114a65
Danny Calise <danny.calise@us.kbr.com>
2024-06-30 15:35:00 -0400

    Items Table

---
+++ Models/Task.cs
@@ -0,0 +1,10 @@
+// Models/Task.cs
+namespace RazorPagesApp.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string AssignedTo { get; set; }
+        public string Name { get; set; }
+    }
+}
